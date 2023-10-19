ad59842470b16471dcd1965ea0d8d6694e296343
v <15816056+fqd511@users.noreply.github.com>
2023-10-18 23:06:32 -0500

    feat(zx): backup.sh
    * add detect for raycast config file
    * add backup action under root path

--- src/zx/backup.sh
+++ src/zx/backup.sh
@@ -17,8 +17,38 @@ mkdir ${date}
 
 cd ${date}
 
-mkdir GoogleAuthenticator
-
 cp ~/.zshrc ~/Downloads/${date}/.zshrc
 
 brew bundle dump --file=~/Downloads/${date}/Brewfile
+
+
+# update Brewfile under ~
+cd ~
+rm Brewfile
+brew bundle dump
+
+## 检测一下有没有 Raycast 备份文件
+
+# 使用shopt -s nullglob保留未匹配的文件列表
+shopt -s nullglob
+# files=(*)直接获取一级目录下文件
+files=(*) 
+
+has_raycast_backup_file=false
+# "${files[@]}"循环遍历文件列表
+for file in "${files[@]}"; do
+  if [[ $file = *".rayconfig" ]]; then
+    has_raycast_backup_file=true
+    break
+  fi
+done
+
+if ! $has_raycast_backup_file; then
+  echo "当前一级目录下没有 Raycast 备份文件"
+  exit 1
+fi
+
+# 执行 backupDotfiles 备份到 GitHub
+./backupDotfiles.sh
+
+echo 'dotFile created and executed backupDotfiles.sh'
