59f4c759b4c8d9d47d4424e036d6c0cfc41ed480
fqd <15816056+fqd511@users.noreply.github.com>
2024-03-17 11:27:47 +0800

    refactor(script): teleCheckin v2.0

--- src/script/teleCheckin.js
+++ src/script/teleCheckin.js
@@ -6,41 +6,49 @@
 var oneMinute = 1000 * 60; // one min as interval
 var intervalInMin = 60 * 6; // base interval: 6 hours
 var bufferInMin = 5; // random buffer: 1-5 mins
-var initialDelayInMin = 0; // delay before 1st execute
+var timeGap = oneMinute * (intervalInMin + (Math.trunc(Math.random() * bufferInMin))); // relative time range
+var lastClickTimeStamp = new Date('2024/3/17 04:43').getTime()
 
-// for web k version
+// click and update lastClickTimeStamp/timeGap
 function clickMenu() {
-	var menuButton = document.querySelector('#column-center > div.chats-container.tabs-container > div > div.chat-input.chat-input-main > div > div.rows-wrapper-wrapper > div > div.new-message-wrapper.rows-wrapper-row > button.btn-icon.toggle-reply-markup.float.show');
-	menuButton.click();
-	setTimeout(() => {
-		var bonusButton = document.querySelector('#column-center > div.chats-container.tabs-container > div > div.chat-input.chat-input-main > div > div.rows-wrapper-wrapper > div > div.reply-keyboard.active > div > div:nth-child(3) > button:nth-child(2)');
-		bonusButton.click();
-		console.log('clicked at', new Date().toLocaleString());
-	}, 555)
+    var menuButton = document.querySelector('#column-center > div.chats-container.tabs-container > div > div.chat-input.chat-input-main > div > div.rows-wrapper-wrapper > div > div.new-message-wrapper.rows-wrapper-row > button.btn-icon.toggle-reply-markup.float.show');
+    if (menuButton) {
+        menuButton.click();
+        setTimeout(() => {
+            var bonusButton = document.querySelector('#column-center > div.chats-container.tabs-container > div > div.chat-input.chat-input-main > div > div.rows-wrapper-wrapper > div > div.reply-keyboard.active > div > div:nth-child(3) > button:nth-child(2)');
+            if (bonusButton) {
+                bonusButton.click();
+                console.log(`%c Click:${new Date().toLocaleString()}`, 'color:green;font-weight:bold;');
+
+                // update variables
+                lastClickTimeStamp = Date.now();
+                timeGap = oneMinute * (intervalInMin + (Math.trunc(Math.random() * bufferInMin)));
+            } else {
+                console.log(`%c Click fail`, 'color:red;font-weight:bold;');
+            }
+        }, 555)
+    } else {
+        console.log(`%c No Menu Button`, 'color:red;font-weight:bold;');
+    }
 }
 
-function startCountdown() {
-	// 设置倒计时时间（单位为毫秒）
-	var countdownTime = oneMinute * (intervalInMin + (Math.trunc(Math.random() * bufferInMin))); // relative time range
-
-	// 开始倒计时
-	var countdown = setInterval(function () {
-		countdownTime -= oneMinute;
-		console.log(new Date().toLocaleString(), "剩余时间：" + countdownTime / oneMinute + "分钟");
-
-		// 倒计时结束时点击特定元素
-		if (countdownTime <= 0) {
-			clearInterval(countdown); // 清除倒计时
-			console.log("倒计时结束，点击");
-			clickMenu();
-			// 重新开始倒计时
-			startCountdown();
-		}
-	}, oneMinute); // every minute
+function execute() {
+    var current = Date.now();
+    const timeLeft = (lastClickTimeStamp + timeGap) - current;
+    if (timeLeft > 1000) {
+        console.log(new Date().toLocaleString(), "剩余：" + ((timeLeft) / oneMinute).toFixed(2) + "分");
+    } else {
+        console.log("倒计时结束，点击");
+        clickMenu();
+    }
 }
 
-setTimeout(() => {
-	clickMenu();
-	// 启动倒计时
-	startCountdown();
-}, oneMinute * initialDelayInMin)
+lastClickTimeStamp = new Date('2024/3/17 04:43').getTime()
+execute();
+setInterval(execute, oneMinute); // every minute
+
+console.log(`%c oneMinute:${oneMinute / 1000} s`, 'color:green;font-weight:bold;');
+console.log(`%c intervalInMin:${intervalInMin} min`, 'color:green;font-weight:bold;');
+console.log(`%c lastClickTimeStamp:${new Date(lastClickTimeStamp).toLocaleString()}(${lastClickTimeStamp})`, 'color:green;font-weight:bold;');
+console.log(`%c timeGap:${Math.trunc(timeGap / oneMinute / 60)}hours${timeGap / oneMinute % 60}min`, 'color:green;font-weight:bold;');
+console.log(`%c 剩余:${(((lastClickTimeStamp + timeGap) - Date.now()) / oneMinute).toFixed(2)}min`, 'color:green;font-weight:bold;');
